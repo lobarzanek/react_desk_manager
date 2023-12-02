a9d77d8c561a6603fd1bfb5e0031c0f1780a768f
lobarzanek <lukiob@gmail.com>
2023-12-02 09:09:31 +0100

    Delete unused function

--- src/components/datePicker/datePicker.jsx
+++ src/components/datePicker/datePicker.jsx
@@ -9,10 +9,8 @@ class DatePicker extends Component {
     const year = today.getFullYear();
     const month = String(today.getMonth() + 1).padStart(2, "0");
     const day = String(today.getDate()).padStart(2, "0");
-
     const formattedDate = `${year}-${month}-${day}`;
 
-    console.log(formattedDate);
     this.setState({ minValue: formattedDate });
   };
 
