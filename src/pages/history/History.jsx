deed807339f7293426191532295fd8716ceae356
lobarzanek <lukiob@gmail.com>
2023-12-02 17:15:07 +0100

    Added History View

--- src/pages/history/History.jsx
+++ src/pages/history/History.jsx
@@ -1,10 +1,38 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Table from "../../components/table/Table";
+
 import "./history.scss";
 
-class History extends Component {
-  render() {
-    return <div>History</div>;
-  }
-}
+const History = () => {
+  const userId = 1;
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    const getTableData = async () => {
+      try {
+        await axios
+          .get(`http://localhost:8000/history?userId=${userId}`)
+          .then((response) => {
+            if (response.statusText === "OK") {
+              setTableData(response.data.slice(0).reverse());
+            }
+          });
+      } catch (error) {
+        console.error("Error fetching table data:", error);
+      }
+    };
+
+    getTableData();
+  }, []);
+
+  return (
+    <div className="history">
+      <div className="wrapper">
+        <Table tableData={tableData} />
+      </div>
+    </div>
+  );
+};
 
 export default History;
