767aa2c6dac7fbc73ec243efb1e2760ea3fbd31d
lobarzanek <lukiob@gmail.com>
2023-12-02 09:04:40 +0100

    Added datePicker component

---
+++ src/components/datePicker/datePicker.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import "./datePicker.scss";
+
+class DatePicker extends Component {
+  state = { minValue: "", value: "" };
+
+  SetCurrentDate = () => {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, "0");
+    const day = String(today.getDate()).padStart(2, "0");
+
+    const formattedDate = `${year}-${month}-${day}`;
+
+    console.log(formattedDate);
+    this.setState({ minValue: formattedDate });
+  };
+
+  handleValueChange = (event) => {
+    const selectedValue = event.target.value;
+    this.setState({ value: selectedValue });
+  };
+
+  componentDidMount() {
+    this.SetCurrentDate();
+  }
+  render() {
+    return (
+      <input
+        className="datePicker"
+        type="date"
+        name="trip-start"
+        defaultValue={this.state.valminValueue}
+        min={this.state.minValue}
+        onChange={this.handleValueChange}
+      />
+    );
+  }
+}
+
+export default DatePicker;
