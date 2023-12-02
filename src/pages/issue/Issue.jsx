3f501a22e608d4bd5ca8a2478126bc72b05309ad
lobarzanek <lukiob@gmail.com>
2023-12-02 18:31:48 +0100

    Added view for issue report

--- src/pages/issue/Issue.jsx
+++ src/pages/issue/Issue.jsx
@@ -1,10 +1,43 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import ChooseBox from "../../components/chooseBox/ChooseBox";
+import Button from "../../components/button/Button";
 import "./issue.scss";
 
-class Issue extends Component {
-  render() {
-    return <div>issue</div>;
+const Issue = () => {
+  const [selectedId, setSelectedId] = useState({
+    floor: "",
+    room: "",
+  });
+
+  const handleBoxChange = (selectedType, selectedValue) => {
+    setSelectedId((prevOptions) => ({
+      ...prevOptions,
+      [selectedType]: selectedValue,
+    }));
+  };
+  {
+    return (
+      <div className="issue">
+        <div className="choose-boxes">
+          <ChooseBox type="floor" onChange={handleBoxChange} />
+          <ChooseBox
+            type="room"
+            selectedId={selectedId.floor}
+            onChange={handleBoxChange}
+          />
+          <ChooseBox
+            type="desk"
+            selectedId={selectedId.room}
+            onChange={handleBoxChange}
+          />
+          <Button text={"zgłoś usterke"} />
+        </div>
+        <div className="text-area">
+          <textarea placeholder="Opisz tutaj swoją usterke"></textarea>
+        </div>
+      </div>
+    );
   }
-}
+};
 
 export default Issue;
