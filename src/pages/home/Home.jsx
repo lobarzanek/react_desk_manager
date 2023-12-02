a7f78108f5c34d707056607336a3cd02d130969e
lobarzanek <lukiob@gmail.com>
2023-12-02 13:54:38 +0100

    Update Home Component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,43 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import ChooseBox from "../../components/chooseBox/ChooseBox";
+import Button from "../../components/button/Button";
+import DatePicker from "../../components/datePicker/datePicker";
 import "./home.scss";
 
-class Home extends Component {
-  render() {
-    return <div>home</div>;
+const Home = () => {
+{
+    const [selectedId, setSelectedId] = useState({
+      floor: "",
+      room: "",
+    });
+
+    const handleBoxChange = (selectedType, selectedValue) => {
+      setSelectedId((prevOptions) => ({
+        ...prevOptions,
+        [selectedType]: selectedValue,
+      }));
+    };
+
+    return (
+      <div className="home">
+        <div className="choose-boxes">
+          <DatePicker />
+          <ChooseBox type="floor" onChange={handleBoxChange} />
+          <ChooseBox
+            type="room"
+            selectedId={selectedId.floor}
+            onChange={handleBoxChange}
+          />
+          <ChooseBox
+            type="desk"
+            selectedId={selectedId.room}
+            onChange={handleBoxChange}
+          />
+          <Button text={"Rezerwuj"} />
+        </div>
+        <div className="desk-map">desk map</div>
+      </div>
+    );
   }
 }
 
