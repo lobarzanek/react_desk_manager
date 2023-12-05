dfc0f3b0715c6b7cb97a0262a7be20b845e6cf8c
lobarzanek <lukiob@gmail.com>
2023-12-05 22:43:19 +0100

    Added Desk map to Home and bind to choose Box

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,43 +2,55 @@ import React, { useState } from "react";
 import ChooseBox from "../../components/chooseBox/ChooseBox";
 import Button from "../../components/button/Button";
 import DatePicker from "../../components/datePicker/datePicker";
+import DeskMap from "../../components/deskMap/DeskMap";
 import "./home.scss";
 
 const Home = () => {
-{
+  {
     const [selectedId, setSelectedId] = useState({
-      floor: "",
-      room: "",
+      floor: 0,
+      room: 0,
+      desk: 0,
     });
 
     const handleBoxChange = (selectedType, selectedValue) => {
       setSelectedId((prevOptions) => ({
         ...prevOptions,
         [selectedType]: selectedValue,
+        
       }));
     };
 
     return (
       <div className="home">
-        <div className="choose-boxes">
-          <DatePicker />
-          <ChooseBox type="floor" onChange={handleBoxChange} />
-          <ChooseBox
-            type="room"
-            selectedId={selectedId.floor}
-            onChange={handleBoxChange}
-          />
-          <ChooseBox
-            type="desk"
-            selectedId={selectedId.room}
-            onChange={handleBoxChange}
-          />
-          <Button text={"Rezerwuj"} />
+        <div className="wrapper">
+          <div className="choose-boxes">
+            <DatePicker />
+            <ChooseBox type="floor" onChange={handleBoxChange} />
+            <ChooseBox
+              type="room"
+              selectedId={selectedId.floor}
+              onChange={handleBoxChange}
+            />
+            <ChooseBox
+              type="desk"
+              selectedId={selectedId.room}
+              secondId={selectedId.desk}
+              onChange={handleBoxChange}
+            />
+            <Button text={"Rezerwuj"} />
+          </div>
+          <div className="shadow-box">
+            <DeskMap
+              roomId={selectedId.room}
+              deskId={selectedId.desk}
+              onChange={handleBoxChange}
+            />
+          </div>
         </div>
-        <div className="desk-map">desk map</div>
       </div>
     );
   }
-}
+};
 
 export default Home;
